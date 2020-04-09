49f5fbd7ce47ae91f2b4527c96bd7456674947a2
Даниил Щербаков <hhat901y@gmail.com>
2020-04-09 10:58:41 +0500

    Добавьте файлы проекта.

---
+++ Dynamic_array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DynamicArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<short> digits = new List<short>() { 45, 12, 52, 71 };
+
+            digits.Add(4);
+
+            digits.AddRange(new short[] { 7, 3, 5, 3 });
+
+            digits.Insert(1, 178);
+
+            digits.Remove(12);
+
+            digits.RemoveAt(2);
+
+            digits.Sort();
+
+            Console.WriteLine(digits[2]);
+
+            Console.ReadKey();
+        }
+    }
+}
